16154a29dc45b67bc51bb04cd811449c369e0fe7
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2021-10-12 12:24:18 -0300

    Audio a ser reproduzido

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var body = document.querySelector('body');
 var inter = document.querySelector('#interruptor');
 var lamp = document.querySelector('#lamp');
 var corda = document.querySelector('#corda');
+var audio = new Audio();
+audio.src = 'interruptor.mp3';
 
 var isOn = false; 
 
 inter.onclick = function() {
     if(isOn == false) {
+        audio.play();
         body.style.background = 'radial-gradient(rgb(170,170,170), rgb(110,110,110), rgb(40,40,40))';
         inter.style.background = 'linear-gradient(to top, rgb(230,230,230), rgb(150,150,150))';
         lamp.style.background = 'radial-gradient(rgb(210,210,210),rgb(110, 110, 110))';
@@ -14,6 +17,7 @@ inter.onclick = function() {
         isOn = true;
     }
     else if(isOn == true) {
+        audio.play();
         body.style.background = 'radial-gradient(rgb(40,40,40), rgb(0,0,0))';
         inter.style.background = 'linear-gradient(to bottom, rgb(230,230,230), rgb(150,150,150))';
         lamp.style.background = 'radial-gradient(rgb(40,40,40),rgb(90, 90, 90))';
